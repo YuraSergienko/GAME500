a2a348c40bbeaa700a57fc5ed86ed3e2c9234b8a
SDHB <yura_sergienko@mail.ru>
2024-09-20 16:55:47 +0600

    update

--- README.md
+++ README.md
@@ -1,16 +1,34 @@
 # GAME500
-ROM cartridge for Vector-06C home computer
+
+![Image](game500.jpg)
+
+ROM картридж для компьютера "Вектор-06Ц"
 
 GAME500 - простое устройство для быстрой загрузки любимых программ. Для подключения к "Вектору" использется порт ПУ, а также при необходимости
 магнитофонный вход. GAME500 эмулирует ROM картридж поддержка которого есть в большенстве загрузочных ПЗУ. Если же в "Векторе" установлено
 заводское загрузочное ПЗУ без поддержки ROM картриджа, на GAME500 есть аудиовыход, который нужно поделючить к магнитофонному разъему "Вектора".
 При нажатии кнопки "PLAY" на GAME500, с него воспроизводится небольшая программка-загрузчик с поддержкой ROM картриджей.
 
-Папка "DOC" в данном репозитории содержит всё необходимое для производства такого картритджа.
-Если вы хотите составить свой список программ для записи их во флэш память GAME500, в папке FLASH находится программа на Phyton для формирования
-файла прошивки. Описание работы программы _flash.py находится в её первых строках.
-В папке "3D" находятся spep файлы для печати корпуса картриджа, кнопочных толкателей и корпуса разъема.
+Содержание папок репозитория:
+box - stl модели корпусных деталей картриджа.
+doc - документация на изделие.
+flash - программа и файлы для подготовки bin файла для записи в флэш память W25Q128.
+gerber - GERBER файлы для производства печатной платы.
+hex - файл прошивки микроконтроллера CH32V103C8T6.
 
 
+ROM cartridge for the Vector-06C computer
+
+GAME500 is a simple device for quickly loading your favorite programs. The PU port is used to connect to the Vector, and if necessary,
+the tape recorder input. GAME500 emulates a ROM cartridge, which is supported by most boot ROMs. If the Vector has a factory boot ROM without
+ROM cartridge support, the GAME500 has an audio output that needs to be connected to the Vector's tape recorder connector.
+When you press the "PLAY" button on the GAME500, a small bootloader program with ROM cartridge support is played from it.
+
+Repository folder contents:
+box - stl models of cartridge body parts.
+doc - product documentation.
+flash - program and files for preparing a bin file for writing to the W25Q128 flash memory.
+gerber - GERBER files for the production of the printed circuit board.
+hex - firmware file for the CH32V103C8T6 microcontroller.
 
 
---
+++ flash/_make_flash.py
@@ -0,0 +1,173 @@
+# для работы программы необходимо создать файл _list.txt
+# файл _list.txt содержит имя файла и название через пробел
+# имя файла не должно содержать пробелы
+# название может содержать пробелы, длина названия не более 16 символов
+# каждая строка должна заканчиваться ENTER
+# в результате успешной работы программы появится файл _flash.bin для записи в микросхему W25Q128
+
+import os.path
+import codecs
+
+lines = sum(1 for line in open('_list.txt'))-1
+print(lines)
+
+temp_file = open('_list.txt', 'r', errors="ignore")
+n_line = sum(1 for line in temp_file)
+temp_file.close()
+
+list_file = open("_list.txt", "r", errors="ignore")
+flash_file = open('_flash.bin', 'wb')
+game_len=0
+fat=bytearray(10020)
+offset_file = 0
+
+for z in range(n_line):
+    print('file number:',z)
+    s = list_file.readline()                # читаем строку из файла
+    print(s)
+    if s != '':                             # если строка пустая то пропускаем
+        x1 = s.find("\n")                   # ищем позицию конца строки (enter)
+        data = s[:x1]                       # отрезаем enter от строки
+        x2 = s.find(" ")                    # ищем позицию пробела между именем файла и названием
+        if x2 < 1:                          # пробел не найден значит нет названия игры
+            file_name = data
+            print(file_name)                # название будет такое же как имя файла
+            game_name = file_name.ljust(15) # дополняем пробелами название игры до 15 символов
+        else:                               # название есть
+            file_name = data[: x2]          # выделяем имя файла
+            print(file_name)
+            game_name = data[x2+1:]         # выделяем название игры
+            game_name = game_name.ljust(15) # дополняем название пробелами до 15
+        s=''
+        filesize = os.path.getsize(file_name)
+
+        g = game_name.encode('cp866')
+#        print(g)
+        for i in range(15):                 # преобразуем каждый символ в шестандцетеричный код
+            b=g[i: i+1]
+#            print(b)
+            if   b==b'\x80': b=0x80 # А
+            elif b==b'\x81': b=0x81 # Б
+            elif b==b'\x82': b=0x82 # В
+            elif b==b'\x83': b=0x83 # Г
+            elif b==b'\x84': b=0x84 # Д
+            elif b==b'\x85': b=0x85 # Е
+            elif b==b'\xf0': b=0xf0 # Ё
+            elif b==b'\x86': b=0x86 # Ж
+            elif b==b'\x87': b=0x87 # З
+            elif b==b'\x88': b=0x88 # И
+            elif b==b'\x89': b=0x89 # Й
+            elif b==b'\x8a': b=0x8a # К
+            elif b==b'\x8b': b=0x8b # Л
+            elif b==b'\x8c': b=0x8c # М
+            elif b==b'\x8d': b=0x8d # Н
+            elif b==b'\x8e': b=0x8e # О
+            elif b==b'\x8f': b=0x8f # П
+            elif b==b'\x90': b=0x90 # Р
+            elif b==b'\x91': b=0x91 # С
+            elif b==b'\x92': b=0x92 # Т
+            elif b==b'\x93': b=0x93 # У
+            elif b==b'\x94': b=0x94 # Ф
+            elif b==b'\x95': b=0x95 # Х
+            elif b==b'\x96': b=0x96 # Ц
+            elif b==b'\x97': b=0x97 # Ч
+            elif b==b'\x98': b=0x98 # Ш
+            elif b==b'\x99': b=0x99 # Щ
+            elif b==b'\x9a': b=0x9a # Ъ
+            elif b==b'\x9b': b=0x9b # Ы
+            elif b==b'\x9c': b=0x9c # Ь
+            elif b==b'\x9d': b=0x9d # Э
+            elif b==b'\x9e': b=0x9e # Ю
+            elif b==b'\x9f': b=0x9f # Я
+            elif b==b'\xa0': b=0xa0 # а
+            elif b==b'\xa1': b=0xa1 # б
+            elif b==b'\xa2': b=0xa2 # в
+            elif b==b'\xa3': b=0xa3 # г
+            elif b==b'\xa4': b=0xa4 # д
+            elif b==b'\xa5': b=0xa5 # е
+            elif b==b'\xf1': b=0xf1 # ё
+            elif b==b'\xa6': b=0xa6 #  ж
+            elif b==b'\xa7': b=0xa7 # з
+            elif b==b'\xa8': b=0xa8 # и
+            elif b==b'\xa9': b=0xa9 # й
+            elif b==b'\xaa': b=0xaa # к
+            elif b==b'\xab': b=0xab # л
+            elif b==b'\xac': b=0xac # м
+            elif b==b'\xad': b=0xad # н
+            elif b==b'\xae': b=0xae # о
+            elif b==b'\xaf': b=0xaf # п
+            elif b==b'\xe0': b=0xe0 # р
+            elif b==b'\xe1': b=0xe1 # с
+            elif b==b'\xe2': b=0xe2 # т
+            elif b==b'\xe3': b=0xe3 # у
+            elif b==b'\xe4': b=0xe4 # ф
+            elif b==b'\xe5': b=0xe5 # х
+            elif b==b'\xe6': b=0xe6 # ц
+            elif b==b'\xe7': b=0xe7 # ч
+            elif b==b'\xe8': b=0xe8 # ш
+            elif b==b'\xe9': b=0xe9 # щ
+            elif b==b'\xea': b=0xea # ъ
+            elif b==b'\xeb': b=0xeb # ы
+            elif b==b'\xec': b=0xec # ь
+            elif b==b'\xed': b=0xed # э
+            elif b==b'\xee': b=0xee # ю
+            elif b==b'\xef': b=0xef # я
+            elif b==b'\xfc': b=0xfc # №
+            else : b=ord(game_name[i: i+1])
+            fat[z*20+i] = b
+
+        print('offset in file:',hex(offset_file))
+        offset_a = offset_file//0x2000
+        print('offset/0x2000:', offset_a)
+        offset_h = offset_a//0x100
+        print('offset_h:', offset_h)
+        offset_l = offset_a - offset_h * 0x100
+        print('offset_l:', offset_l)
+        fat[z*20+15] = 0x00
+        fat[z*20+16] = offset_h
+        fat[z*20+17] = offset_l
+        if filesize > 0x9fff:
+            game_len=0x06
+        elif filesize > 0x7fff:
+            game_len=0x05
+        elif filesize > 0x5fff:
+            game_len=0x04
+        elif filesize > 0x3fff:
+            game_len=0x03
+        elif filesize > 0x1fff:
+            game_len=0x02
+        elif filesize > 0x0000:
+            game_len=0x01
+        fat[z*20+18] = game_len
+        print('game len in 0x2000:',game_len,'\n')
+
+# копирование файла игры в флэш файл
+        game_file = open(file_name, 'rb')
+        d = game_file.read()
+        flash_file.seek(offset_file+0x100, 0)
+        flash_file.write(bytearray(d))
+        game_file.close()
+    else:
+        for k in range(20):
+            fat[z*20+k] = 0x20
+
+    offset_file = offset_file + game_len * 0x2000
+
+# запись файловой базы в флэш файл
+flash_file.seek(0x1300)
+flash_file.write(fat)
+flash_file.close()  
+# запись файловой базы в файл 000_loader3.rom
+flash_file = open('000_loader3.rom', 'rb+')
+flash_file.seek(0x1200)
+flash_file.write(fat)
+flash_file.close()
+
+list_file.close()
+print("\n")
+print("_flash.bin creation complied")
+
+
+
+
+
